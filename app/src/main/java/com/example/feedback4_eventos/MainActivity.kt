b8c8c5511d972a4304344250abbebf52c02f0d66
jmartter <150058251+jmartter@users.noreply.github.com>
2024-11-10 23:14:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/feedback4_eventos/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.feedback4_eventos
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.feedback4_eventos.Inicio.LoginActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inicializar Firestore
+        val db = FirebaseFirestore.getInstance()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
